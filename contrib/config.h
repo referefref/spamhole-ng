3cc5fc40245e007549b81cd94eb93daa1de55dcb
ref <56499429+referefref@users.noreply.github.com>
2024-05-15 16:33:33 +0800

    Create config.h

---
+++ contrib/config.h
@@ -0,0 +1,12 @@
+/* Configuration Parameters */
+
+/* Debug Logging Level */
+#define DEBUG 1
+
+/* UID to run as */
+#define UID 99
+
+/* Listening interface address and port*/
+#define LOCAL_IP "0.0.0.0"
+#define LOCAL_PORT "25"
+
